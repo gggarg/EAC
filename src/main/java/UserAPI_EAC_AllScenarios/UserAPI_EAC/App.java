87eaed2d7cc326554790d794a71975b0743520c0
Akanksha Kalla <AKalla@svam.com>
2018-02-01 15:31:45 +0530

    initial

---
+++ src/main/java/UserAPI_EAC_AllScenarios/UserAPI_EAC/App.java
@@ -0,0 +1,13 @@
+package UserAPI_EAC_AllScenarios.UserAPI_EAC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
